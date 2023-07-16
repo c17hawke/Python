3aeb5d29a4f1981dd1db30c8f03d25fcef23447c
c17hawke <sunny.c17hawke@gmail.com>
2023-07-16 18:58:39 +0530

    demo code for lecture 001

---
+++ docs/noteboooks/001-getting-started/demo.py
@@ -0,0 +1,19 @@
+print("Hello, World!")
+print(4+2)
+# This is a comment
+
+"""
+This is a 
+mulitline comment
+"""
+
+'''The following code is simple a
+mathematical calculation'''
+print("Sum of 4 and 2=", 4+2)
+print("Product of 4 and 2=", 4*2)
+
+# The following code will give Syntax error
+# print("Product of 4 and 2=", 4*2
+
+# The following code will give exception Zero Division Error
+# print("Division of 4 and 0=", 4/0)
