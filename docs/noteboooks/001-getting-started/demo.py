21ce1ad5299351f239fbc0e4a05cab529e8650b5
c17hawke <sunny.c17hawke@gmail.com>
2023-08-19 16:33:46 +0530

    udpated with more details

--- docs/noteboooks/001-getting-started/demo.py
+++ docs/noteboooks/001-getting-started/demo.py
@@ -1,19 +1,53 @@
-print("Hello, World!")
-print(4+2)
-# This is a comment
+"""
+Author: SUNNY BHAVEEN CHANDRA
+For more information - 
+[1] Python notes-
+    https://c17hawke.github.io/Python/
+
+[2] Python Playlist- 
+    https://youtube.com/playlist?list=PLrdaCCBhU_hnxIzB7EJlY-pfYOMGRycAK
 
 """
-This is a 
-mulitline comment
+
+print("Hello, world!")
+print("Hello, Python enthusiasts!")
+
+print(4 + 2)
+
+# COMMENTS
+# The following code is simple
+# mathematical calculation
+print("Sum of 4 and 2 =", 4 + 2)
+print("product of 4 and 2 =", 4 * 2)
+
+
+"""
+MULTI-LINE COMMENT
+The following code is simple
+mathematical calculation
+"""
+print("Sum of 4 and 2 =", 4 + 2)
+print("product of 4 and 2 =", 4 * 2)
+
+"""
+Errors / Exception in python:
+
+When something goes wrong in your Python code, an error or exception occurs.
+
+1. Errors:
+An error is a mistake in the code that prevents the program from running correctly.
+
+2. Exception:
+An exception, on the other hand, is an event that occurs during the execution of a program that disrupts the normal flow of the program's instructions.
 """
 
-'''The following code is simple a
-mathematical calculation'''
-print("Sum of 4 and 2=", 4+2)
-print("Product of 4 and 2=", 4*2)
 
-# The following code will give Syntax error
-# print("Product of 4 and 2=", 4*2
+# # The following code will give Syntax error
+# print("Hello, world!")
+# print("Division of x and y", 2/3 # <-- Error
+# print("Welcome to the world of Python programming")
 
-# The following code will give exception Zero Division Error
-# print("Division of 4 and 0=", 4/0)
+# # The following code will give exception Zero Division Error
+# print("Hello, world!")
+# print("Division of x and y", 2/0) # <-- Exception
+# print("Welcome to the world of Python programming")
